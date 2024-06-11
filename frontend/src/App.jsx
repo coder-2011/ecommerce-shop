fe31a6b64e72c32b1094ffe0835464057266d7fb
coder-2011 <65727553+coder-2011@users.noreply.github.com>
2024-06-11 12:40:20 +0530

    Footer Component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import {Container} from 'react-bootstrap';
+import Header from "./components/Header.jsx";
+import Footer from './components/Footer.jsx';
+const App = () => {
+  return (
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <h1>Welcome to Proshop</h1>
+      </Container>
+    </main>
+    <Footer />
+    </>
+  )
+}
+
+export default App;
