6bfb5003db6ca061a7a4457e2ea595151d3351e7
coder-2011 <65727553+coder-2011@users.noreply.github.com>
2024-06-13 02:14:31 +0530

    Added React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import {Container} from 'react-bootstrap';
-import Header from "./components/Header.jsx";
-import Footer from './components/Footer.jsx';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
     <>
-    <Header />
-    <main className='py-3'>
-      <Container>
-        <h1>Welcome to Proshop</h1>
-      </Container>
-    </main>
-    <Footer />
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App;
