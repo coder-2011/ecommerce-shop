c882bf22875ecde93553526a5af3e7f855b4d3d2
coder-2011 <65727553+coder-2011@users.noreply.github.com>
2024-08-06 13:54:11 -0700

    Cart Reducer functions/setup

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,29 +1,30 @@
-import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import axios from 'axios';
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { useState } from 'react';
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
-  const [product, setProduct] = useState({});
+  const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
+  const [qty, setQty] = useState(1);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    };
 
-    fetchProduct();
-  }, [productId]);
 
   return (
     <>
       <Link to='/' className='btn btn-light my-3'>
         Go Back
       </Link>
-      <Row>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error?.data?.message || error.error}</Message>
+      ) : (<Row>
         <Col md={5}>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -63,6 +64,8 @@ const ProductScreen = () => {
                 </Row>
               </ListGroup.Item>
 
+            {product.countInStock > 0 && (
+
               <ListGroup.Item>
                 <Button
                   className='btn-block'
@@ -76,6 +79,8 @@ const ProductScreen = () => {
           </Card>
         </Col>
       </Row>
+    )}
+      
     </>
   );
 };
